ec6f65768fe81f19631d245f461259401b8b8bb9
Kevin Breinert <98883117+kevinbreinert@users.noreply.github.com>
2023-05-08 16:15:41 +0200

    database

--- Database/database.php
+++ Database/database.php
@@ -1,32 +1,49 @@
 <?php
-class Connection {
+class Connection{
+
+    //Attribute
     private string $host = 'bfi.bbs-me.org';
     private string $username = 'lendme';
     private string $password = 'lendme123';
     private string $database = 'lendme';
 
     private int $port = 1568;
+
     private $conn;
 
-    public function __construct() {}
+    // Leerer Konstruktor
+    public function __construct(){
 
-    public function connect(): mysqli {
+    }
+
+    // Verbindung zur Datenbank
+    public function connect(): mysqli
+    {
         $this->conn = new mysqli($this->host, $this->username, $this->password, $this->database, $this->port);
+
         if ($this->conn->connect_error) {
             die("Verbindung zur Datenbank fehlgeschlagen: " . $this->conn->connect_error);
         }
+
         return $this->conn;
     }
 
+
+    // Methoden
+
+    // Abfrage SQL
     public function query($query) {
         $result = $this->conn->query($query);
         if (!$result) {
-            die("Abfrage fehlgeschlagen: " . mysqli_error($this->conn));
+            die("Abfrage fehlgeschlagen: " . $this->conn->error);
         }
         return $result;
     }
 
-    public function close() {
+
+    // Verbindung schließen
+    public function close(): void
+    {
         $this->conn->close();
     }
 }
